6f2f9dc12d6a080013aa9b54fbe61ccdc054132d
raksana04 <69308795+raksana04@users.noreply.github.com>
2020-08-21 13:19:44 +0200

    Update Uhr.py

--- Uhr.py
+++ Uhr.py
@@ -1,136 +1,87 @@
-#!/usr/bin/env python
-#
-# binary_clock.py - A Python implementation of a binary clock
-# for the Pimoroni Scroll Bot.
-# Copyright (C) 2018 Freddy Spierenburg
+mport time
 
 import scrollphathd
-import datetime
-import math
-import random
+from scrollphathd.fonts import font5x5
 
+print("""
+Scroll pHAT HD: Clock
+Displays hours and minutes in text,
+plus a seconds progress bar.
+Press Ctrl+C to exit!
+""")
 
-class Time(object):
-    def __init__(self):
-        self.update()
+# Display a progress bar for seconds
+# Displays a dot if False
+DISPLAY_BAR = False
 
-    def hour(self):
-        return self._now.hour
+# Brightness of the seconds bar and text
+BRIGHTNESS = 0.3
 
-    def minute(self):
-        return self._now.minute
+# Uncomment the below if your display is upside down
+#   (e.g. if you're using it in a Pimoroni Scroll Bot)
+# scrollphathd.rotate(degrees=180)
 
-    def second(self):
-        return self._now.second
+while True:
+    scrollphathd.clear()
 
-    def update(self):
-        self._now = datetime.datetime.now()
+    # Grab the "seconds" component of the current time
+    # and convert it to a range from 0.0 to 1.0
+    float_sec = (time.time() % 60) / 59.0
 
+    # Multiply our range by 15 to spread the current
+    # number of seconds over 15 pixels.
+    #
+    # 60 is evenly divisible by 15, so that
+    # each fully lit pixel represents 4 seconds.
+    #
+    # For example this is 28 seconds:
+    # [x][x][x][x][x][x][x][ ][ ][ ][ ][ ][ ][ ][ ]
+    #  ^ - 0 seconds                59 seconds - ^
+    seconds_progress = float_sec * 15
 
-class Clock(Time):
-    def __init__(self):
-        Time.__init__(self)
+    if DISPLAY_BAR:
+        # Step through 15 pixels to draw the seconds bar
+        for y in range(15):
+            # For each pixel, we figure out its brightness by
+            # seeing how much of "seconds_progress" is left to draw
+            # If it's greater than 1 (full brightness) then we just display 1.
+            current_pixel = min(seconds_progress, 1)
 
-    def _hour(self):
-        print(self.hour())
+            # Multiply the pixel brightness (0.0 to 1.0) by our global brightness value
+            scrollphathd.set_pixel(y + 1, 6, current_pixel * BRIGHTNESS)
 
-    def _minute(self):
-        print(self.minute())
+            # Subtract 1 now we've drawn that pixel
+            seconds_progress -= 1
 
-    def _second(self):
-        print(self.second())
+            # If we reach or pass 0, there are no more pixels left to draw
+            if seconds_progress <= 0:
+                break
 
-    def draw(self):
-        self._hour()
-        self._minute()
-        self._second()
+    else:
+        # Just display a simple dot
+        scrollphathd.set_pixel(int(seconds_progress), 6, BRIGHTNESS)
 
+    # Display the time (HH:MM) in a 5x5 pixel font
+    scrollphathd.write_string(
+        time.strftime("%H:%M"),
+        x=0,                   # Align to the left of the buffer
+        y=0,                   # Align to the top of the buffer
+        font=font5x5,          # Use the font5x5 font we imported above
+        brightness=BRIGHTNESS  # Use our global brightness value
+    )
 
-class BinaryClock(Clock):
-    def __init__(self):
-        self._SPHD = ScrollPhatHD()
-        self._max_degree = 45
-        self._hand_position = [2, 4, 7, 9, 12, 14]
-        self._hand_bits = 4
-        self._max_intensity = 0.6
-        self._intensities_init(self._max_intensity)
-        Clock.__init__(self)
+    # int(time.time()) % 2 will tick between 0 and 1 every second.
+    # We can use this fact to clear the ":" and cause it to blink on/off
+    # every other second, like a digital clock.
+    # To do this we clear a rectangle 8 pixels along, 0 down,
+    # that's 1 pixel wide and 5 pixels tall.
+    if int(time.time()) % 2 == 0:
+        scrollphathd.clear_rect(8, 0, 1, 5)
 
-    def _draw_binary(self, x, value):
-        for y in range(self._hand_bits):
-            self._SPHD.set_pixel(
-                x,
-                ((self._hand_bits - 1 - y) * 2),
-                1 if value & (1 << y) > 0 else self._brightness[x][y])
-
-    def _draw_hand(self, x_left, x_right, value):
-        self._draw_binary(x_left, value / 10)
-        self._draw_binary(x_right, value % 10)
-
-    def _hour(self):
-        self._draw_hand(self._hand_position[0],
-                        self._hand_position[1],
-                        self.hour())
-
-    def _minute(self):
-        self._draw_hand(self._hand_position[2],
-                        self._hand_position[3],
-                        self.minute())
-
-    def _second(self):
-        self._draw_hand(self._hand_position[4],
-                        self._hand_position[5],
-                        self.second())
-
-    def draw(self):
-        Clock.draw(self)
-        self._SPHD.show()
-
-    def _intensities_init(self, max_intensity):
-        self._intensities = {
-            hand_position: [
-                self._intensity(max_intensity) for hand_bit in range(self._hand_bits)
-            ] for hand_position in self._hand_position
-        }
-
-    def _brightness_step(self):
-        self._brightness = {
-            hand_position: [
-                next(self._intensities[hand_position][hand_bit]) for hand_bit in range(self._hand_bits)
-            ] for hand_position in self._intensities.keys()
-        }
-
-    def _intensity(self, max_intensity):
-        while True:
-            for silence in range(random.randrange(99)):
-                yield 0
-            for degree in range(self._max_degree) + list(reversed(range(self._max_degree))):
-                yield math.tan(math.radians(degree)) * max_intensity
-
-    def update(self):
-        Clock.update(self)
-        self._brightness_step()
-
-
-class ScrollPhatHD(object):
-    def __init__(self):
-        scrollphathd.rotate(180)
-        scrollphathd.clear()
-        self.show()
-
-    def set_pixel(self, x, y, brightness):
-        scrollphathd.set_pixel(x, y, brightness)
-
-    def show(self):
-        scrollphathd.show()
-
-
-def main():
-    clock = BinaryClock()
-    while True:
-        clock.update()
-        clock.draw()
-
-
-if __name__ == '__main__':
-    main()
+    # Display our time and sleep a bit. Using 1 second in time.sleep
+    # is not recommended, since you might get quite far out of phase
+    # with the passing of real wall-time seconds and it'll look weird!
+    #
+    # 1/10th of a second is accurate enough for a simple clock though :D
+    scrollphathd.show()
+    time.sleep(0.1)
